a156ca87f712a38bff890b4338406b84a9a187d7
Ted Mbogo <114946158+Tedmbg@users.noreply.github.com>
2024-08-01 14:08:34 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function createCard(emojipedia) {
+  return (
+    <Card
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      {emojipedia.map(createCard)}
+    </div>
+  );
+}
+
+export default App;
